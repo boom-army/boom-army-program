715cf7d45e33ab0856ddc69c56d261c5afc28cb6
Harkl <3zy3@protonmail.com>
2021-08-17 13:06:51 +1200

    re-init

---
+++ programs/sosol/src/lib.rs
@@ -0,0 +1,131 @@
+//! Sosol intereaction
+
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, TokenAccount, Transfer};
+use std::convert::Into;
+
+#[program]
+pub mod sosol {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
+    }
+
+//     pub fn interaction(ctx: Context<Interaction>, interaction_fee: u64) -> ProgramResult {
+//         // const STORAGE_PERCENT_SPLIT: usize = 10;
+//         // let cpi_accounts = Transfer {
+//         //     from: ctx.accounts.from.to_account_info().clone(),
+//         //     to: ctx.accounts.to.to_account_info().clone(),
+//         //     authority: ctx.accounts.owner.clone(),
+//         // };
+//         // let cpi_program = ctx.accounts.token_program.clone();
+//         // let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+//         // token::transfer(cpi_ctx, interaction_fee)?;
+//         // let to = ctx.accounts.to.to_account_info().clone().key;
+//         // let from = ctx.accounts.from.to_account_info().clone().key;
+//         // let owner = ctx.accounts.owner.to_account_info().clone().key;
+//         // let to_storage_account = ctx.accounts.to_storage_account.to_account_info().clone().key;
+//         // msg!(
+//         //     "to: {}, from: {}",
+//         //     to,
+//         //     from,
+//         //     // owner,
+//         //     // to_storage_account,
+//         //     // interaction_fee,
+//         //     // STORAGE_PERCENT_SPLIT,
+//         // );
+//         // 1 - check balance against interaction_fee
+//         // let from_account_balance = token::accessor::amount(from)?;
+
+//         // 3 - split payment using storage_percent_fee
+
+//         // 2 - pay to creator_account_key
+//         msg!("Calling the token program to transfer tokens to the content creator...");
+//         // 1 . transfer token
+//         // let cpi_accounts = Transfer {
+//         //     from: ctx.accounts.from.to_account_info().clone(),
+//         //     to: ctx.accounts.to.to_account_info().clone(),
+//         //     authority: ctx.accounts.owner.clone(),
+//         // };
+//         // let cpi_program = ctx.accounts.token_program.clone();
+//         // let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+//         // token::transfer(cpi_ctx, interaction_fee)?;
+
+//         // 4 - if has storage_percent_fee -> pay to storage_account_key
+
+//         // // Safety checks.
+//         // apply_risk_checks(DidSwap {
+//         //     authority: *ctx.accounts.authority.key,
+//         //     given_amount: amount,
+//         //     min_expected_swap_amount,
+//         //     from_amount,
+//         //     to_amount,
+//         //     spill_amount: 0,
+//         //     from_mint: token::accessor::mint(from_token)?,
+//         //     to_mint: token::accessor::mint(to_token)?,
+//         //     quote_mint: match side {
+//         //         Side::Bid => token::accessor::mint(from_token)?,
+//         //         Side::Ask => token::accessor::mint(to_token)?,
+//         //     },
+//         // })?;
+
+//         // 6 - clean up / delete accounts
+//         Ok(())
+//     }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init)]
+    pub my_account: ProgramAccount<'info, MyAccount>,
+}
+
+// #[derive(Accounts)]
+// pub struct Interaction<'info> {
+//     #[account(mut, has_one = owner)]
+//     from: CpiAccount<'info, TokenAccount>,
+//     #[account("from.mint == to.mint")]
+//     to: CpiAccount<'info, TokenAccount>,
+//     #[account("from.mint == to_storage_account.mint")]
+//     to_storage_account: CpiAccount<'info, TokenAccount>,
+//     #[account(signer)]
+//     owner: AccountInfo<'info>,
+//     token_program: AccountInfo<'info>,
+// }
+
+#[account]
+pub struct MyAccount {
+    pub data: u64,
+}
+
+// #[account]
+// pub struct SosolAccount {
+//     from: Pubkey,
+//     to: Pubkey,
+//     to_storage_account: Pubkey,
+//     interaction_fee: u64,
+// }
+
+#[error]
+pub enum ErrorCode {
+    #[msg("The given nonce does not create a valid program derived address.")]
+    InvalidInteractionNonce,
+    #[msg("The derived interaction signer does not match that which was given.")]
+    InvalidInteractionSigner,
+    #[msg("The given interaction has already been burned.")]
+    AlreadyBurned,
+}
+
+// // Asserts the swap event is valid.
+// see examples/swap/programs/swap/src/lib.rs : 177
+// fn apply_risk_checks(event: DidSwap) -> Result<()> {
+//     // Reject if the resulting amount is less than the client's expectation.
+//     if event.to_amount < event.min_expected_swap_amount {
+//         return Err(ErrorCode::SlippageExceeded.into());
+//     }
+//     emit!(event);
+//     Ok(())
+// }
---
+++ tests/sosol.js
@@ -0,0 +1,106 @@
+const anchor = require("@project-serum/anchor");
+const serumCmn = require("@project-serum/common");
+
+const { TOKEN_PROGRAM_ID } = require("@solana/spl-token");
+
+describe("sosol-tests", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Sosol;
+  console.log('*******************', program);
+
+  const MINT_TOKENS = 4200000000000000; // 42M with 8dp
+  const MINT_DECIMALS = 8;
+
+  let mint = null;
+  let god = null;
+  let consumerAcc = null;
+  let creatorAcc = anchor.web3.Keypair.generate();
+  let storageAcc = anchor.web3.Keypair.generate();
+
+  it("Sets up initial test state", async () => {
+    const [_mint, _god] = await serumCmn.createMintAndVault(
+      program.provider,
+      new anchor.BN(MINT_TOKENS),
+      undefined,
+      MINT_DECIMALS
+    );
+    mint = _mint;
+    god = _god;
+
+    consumerAcc = god;
+
+    await serumCmn.createTokenAccount(
+      program.provider,
+      mint,
+      creatorAcc.publicKey
+    );
+
+    await serumCmn.createTokenAccount(
+      program.provider,
+      mint,
+      storageAcc.publicKey
+    );
+  });
+
+  it("Actions an interaction", async () => {
+    const INTERACTION_FEE = 1000;
+    const owner = program.provider.wallet.publicKey;
+
+    console.log('*************', {
+      from: program.provider.wallet.publicKey.toBase58(),
+      to: creatorAcc.publicKey.toBase58(),
+      toStorageAccount: storageAcc.publicKey.toBase58(),
+      owner: program.provider.wallet.publicKey.toBase58(),
+      tokenProgram: TOKEN_PROGRAM_ID.toBase58(),
+      programId: program.programId.toBase58(),
+    });
+
+    const myAccount = creatorAcc;
+
+    await program.rpc.initialize(new anchor.BN(1234), {
+      accounts: {
+        myAccount: myAccount.publicKey,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [myAccount],
+      instructions: [await program.account.myAccount.createInstruction(myAccount)],
+    });
+
+    // await program.rpc.interaction(new anchor.BN(INTERACTION_FEE), {
+    //   accounts: {
+    //     from: program.provider.wallet.publicKey,
+    //     to: creatorAcc.publicKey,
+    //     toStorageAccount: storageAcc.publicKey,
+    //     owner: program.provider.wallet.publicKey,
+    //     tokenProgram: TOKEN_PROGRAM_ID,
+    //     interactionFee: INTERACTION_FEE,
+    //   },
+    //   signers: [owner],
+    // });
+
+    // let _initializerTokenAccountA = await mintA.getAccountInfo(initializerTokenAccountA);
+
+    // const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // console.log('****************', account);
+
+    // Check it's state was initialized.
+    // assert.ok(account.data.eq(new anchor.BN(1234)));
+
+    // Check that the new owner is the PDA.
+    // assert.ok(_initializerTokenAccountA.owner.equals(pda));
+
+    // // Check that the values in the escrow account match what we expect.
+    // assert.ok(_escrowAccount.initializerKey.equals(provider.wallet.publicKey));
+    // assert.ok(_escrowAccount.initializerAmount.toNumber() == initializerAmount);
+    // assert.ok(_escrowAccount.takerAmount.toNumber() == takerAmount);
+    // assert.ok(
+    //   _escrowAccount.initializerDepositTokenAccount.equals(initializerTokenAccountA)
+    // );
+    // assert.ok(
+    //   _escrowAccount.initializerReceiveTokenAccount.equals(initializerTokenAccountB)
+    // );
+  });
+});
